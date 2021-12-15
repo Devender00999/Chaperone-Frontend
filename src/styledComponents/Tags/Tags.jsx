c1556f48eed1d10fcedb8efa8f5b1ecbecd4fd18
Devender Kumar <Devender00999@gmail.com>
2021-12-15 13:40:48 +0530

    Updated Tags

--- src/styledComponents/Tags/Tags.jsx
+++ src/styledComponents/Tags/Tags.jsx
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import { Tag, TagContainer } from "./Tags.styles";
 
 const Tags = (props) => {
-  const [tag, setTag] = useState("");
-  console.log(props.tags, tag);
+  const [tagIndex, setTag] = useState(0);
   return (
     <TagContainer>
       {props.tags.map((tag, index) => (
-        <Tag key={index} onClick={() => setTag(index)} selected={index === tag}>
+        <Tag
+          key={index}
+          onClick={() => setTag(index)}
+          selected={tagIndex === index}
+        >
           {tag.value}
         </Tag>
       ))}
