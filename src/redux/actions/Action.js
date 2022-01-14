709cbbafef08e10081b9ff4a820c5b10c0c2d98d
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-14 23:30:48 +0530

    added space 3

--- src/redux/actions/Action.js
+++ src/redux/actions/Action.js
@@ -2,8 +2,8 @@ import * as admissionActions from "./admissionActions";
 import * as userActions from "./UserActions";
 
 const Actions = {
-  ...admissionActions,
-  ...userActions,
+   ...admissionActions,
+   ...userActions,
 };
 
 export default Actions;
--- src/redux/actions/admissionActions.js
+++ src/redux/actions/admissionActions.js
@@ -1,16 +1,16 @@
 import ActionTypes from "../constants/ActionTypes";
 
 export const setAllAdArticles = (articles) => ({
-  type: ActionTypes.SET_ALL_ADMISSION_ARTICLES,
-  payload: articles,
+   type: ActionTypes.SET_ALL_ADMISSION_ARTICLES,
+   payload: articles,
 });
 
 export const setAdArticle = (article) => ({
-  type: ActionTypes.SET_ADMISSION_ARTICLE,
-  payload: article,
+   type: ActionTypes.SET_ADMISSION_ARTICLE,
+   payload: article,
 });
 
 export const likeAdArticle = (id) => ({
-  type: ActionTypes.LIKE_ADMISSION_ARTICLE,
-  payload: { id },
+   type: ActionTypes.LIKE_ADMISSION_ARTICLE,
+   payload: { id },
 });
