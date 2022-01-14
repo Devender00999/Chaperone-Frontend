007f6f8678335de1226d6047a90f8c23370b1dc3
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-15 00:56:19 +0530

    conflicts resolved

--- src/pages/user/HomeScreen.jsx
+++ src/pages/user/HomeScreen.jsx
@@ -48,37 +48,39 @@ const HomeScreen = (props) => {
    const allAdArticles = useSelector((state) => state.allAdArticles);
    console.log(allAdArticles);
 
-  useEffect(() => {
-    async function AdArticleSetup() {
-      const res = await Request.get("http://localhost:" + port + "/api/admissions/");
-      console.log(res);
-      dispatch(Actions.setAllAdArticles(res));
-    }
-    AdArticleSetup();
-  }, [dispatch]);
+   useEffect(() => {
+      async function AdArticleSetup() {
+         const res = await Request.get(
+            "http://localhost:" + port + "/api/admissions/"
+         );
+         console.log(res);
+         dispatch(Actions.setAllAdArticles(res));
+      }
+      AdArticleSetup();
+   }, [dispatch]);
 
-  return (
-    <>
-      <MainContent direction="column" flex={3}>
-        <Tags tags={tags} />
-        {Object.keys(allAdArticles).length === 0
-          ? "Loading..."
-          : allAdArticles.map((blog, id) => (
-            <BlogsCard key={id} {...blog} />
-          ))}
-        {careerData.map((data, id) => (
-          <CareerCard key={id} data={data} />
-        ))}
-        <ProjectCard
-          image="/images/projects/Image.svg"
-          heading="OurApp - a social media web app in NodeJS"
-          desc="Build this full stack application where you will get to learn about building modern, fast and scalable server-side web applications with NodeJS, databases like MongoDB and more."
-          liveLink="/"
-          githubLink="/"
-        />
-        {pgData.map((pg, id) => (
-          <PGCard key={id} data={pg} />
-        ))}
+   return (
+      <>
+         <MainContent direction="column" flex={3}>
+            <Tags tags={tags} />
+            {Object.keys(allAdArticles).length === 0
+               ? "Loading..."
+               : allAdArticles.map((blog, id) => (
+                    <BlogsCard key={id} {...blog} />
+                 ))}
+            {careerData.map((data, id) => (
+               <CareerCard key={id} data={data} />
+            ))}
+            <ProjectCard
+               image="/images/projects/Image.svg"
+               heading="OurApp - a social media web app in NodeJS"
+               desc="Build this full stack application where you will get to learn about building modern, fast and scalable server-side web applications with NodeJS, databases like MongoDB and more."
+               liveLink="/"
+               githubLink="/"
+            />
+            {pgData.map((pg, id) => (
+               <PGCard key={id} data={pg} />
+            ))}
 
             {easyBuyData.map((item, id) => (
                <EasyBuyCard key={id} data={item} />
