2da8ae371ed00f94b18736f0330b766742a0bfb1
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-15 12:24:51 +0530

    fixed admin

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/user/Home";
 import {
-  Content,
-  StyledContainer,
-  StyledMain,
+   Content,
+   StyledContainer,
+   StyledMain,
 } from "./styledComponents/common/Common/Common.styles";
 import Navbar from "./styledComponents/Navbar/Navbar";
 import SideBar from "./styledComponents/SidePanel/SideBar";
@@ -15,161 +15,161 @@ import AdminRoutes from "./routes/admin-routes";
 import UserRoutes from "./routes/user-routes";
 import DefaultRoutes from "./routes/default-routes";
 import getUserDetails from "./requests/decode/decodeToken";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const sideDataForUser = [
-  {
-    title: "Home",
-    link: "/dashboard",
-    icon: "/images/sidebar/home.svg",
-    selected: true,
-  },
-  {
-    title: "Admission",
-    link: "/dashboard/admission",
-    icon: "/images/sidebar/admission.svg",
-    selected: false,
-  },
-  {
-    title: "Roadmaps",
-    link: "/dashboard/roadmaps",
-    icon: "/images/sidebar/roadmap.svg",
-    selected: false,
-  },
-  {
-    title: "Academics",
-    link: "/dashboard/academics",
-    icon: "/images/sidebar/academics.svg",
-    selected: false,
-  },
-  {
-    title: "Career Aware",
-    link: "/dashboard/career-aware",
-    icon: "/images/sidebar/careeraware.svg",
-    selected: false,
-  },
-  {
-    title: "Find PG",
-    link: "/dashboard/find-pg",
-    icon: "/images/sidebar/findpg.svg",
-    selected: false,
-  },
-  {
-    title: "Easy Buy",
-    link: "/dashboard/easy-buy",
-    icon: "/images/sidebar/easybuy.svg",
-    selected: false,
-  },
-  {
-    title: "Doubt Desk",
-    link: "/dashboard/doubt-desk",
-    icon: "/images/sidebar/doubtdesk.svg",
-    selected: false,
-  },
-  // {
-  //   title: "Write a blog",
-  //   link: "/dashboard/blogs",
-  //   icon: "/images/sidebar/blogs.svg",
-  //   selected: false,
-  // },
+   {
+      title: "Home",
+      link: "/dashboard",
+      icon: "/images/sidebar/home.svg",
+      selected: true,
+   },
+   {
+      title: "Admission",
+      link: "/dashboard/admission",
+      icon: "/images/sidebar/admission.svg",
+      selected: false,
+   },
+   {
+      title: "Roadmaps",
+      link: "/dashboard/roadmaps",
+      icon: "/images/sidebar/roadmap.svg",
+      selected: false,
+   },
+   {
+      title: "Academics",
+      link: "/dashboard/academics",
+      icon: "/images/sidebar/academics.svg",
+      selected: false,
+   },
+   {
+      title: "Career Aware",
+      link: "/dashboard/career-aware",
+      icon: "/images/sidebar/careeraware.svg",
+      selected: false,
+   },
+   {
+      title: "Find PG",
+      link: "/dashboard/find-pg",
+      icon: "/images/sidebar/findpg.svg",
+      selected: false,
+   },
+   {
+      title: "Easy Buy",
+      link: "/dashboard/easy-buy",
+      icon: "/images/sidebar/easybuy.svg",
+      selected: false,
+   },
+   {
+      title: "Doubt Desk",
+      link: "/dashboard/doubt-desk",
+      icon: "/images/sidebar/doubtdesk.svg",
+      selected: false,
+   },
+   // {
+   //   title: "Write a blog",
+   //   link: "/dashboard/blogs",
+   //   icon: "/images/sidebar/blogs.svg",
+   //   selected: false,
+   // },
 ];
 
 const sideDataForAdmin = [
-  {
-    title: "Dashboard",
-    link: "/admin",
-    icon: "/images/sidebar/dashboard.svg",
-    selected: true,
-  },
-  {
-    title: "Admissions",
-    link: "/admin/admissions",
-    icon: "/images/sidebar/admission.svg",
-    selected: false,
-  },
-  {
-    title: "Roadmaps",
-    link: "/admin/roadmaps",
-    icon: "/images/sidebar/roadmap.svg",
-    selected: false,
-  },
-  {
-    title: "Projects",
-    link: "/admin/projects",
-    icon: "/images/sidebar/project.svg",
-    selected: false,
-  },
-  {
-    title: "Academics",
-    link: "/admin/academics",
-    icon: "/images/sidebar/academics.svg",
-    selected: false,
-  },
-  {
-    title: "Career Aware",
-    link: "/admin/career-aware",
-    icon: "/images/sidebar/careeraware.svg",
-    selected: false,
-  },
-  {
-    title: "Find PG",
-    link: "/admin/find-pg",
-    icon: "/images/sidebar/findpg.svg",
-    selected: false,
-  },
-  {
-    title: "Easy Buy",
-    link: "/admin/easy-buy",
-    icon: "/images/sidebar/easybuy.svg",
-    selected: false,
-  },
+   {
+      title: "Dashboard",
+      link: "/admin",
+      icon: "/images/sidebar/dashboard.svg",
+      selected: true,
+   },
+   {
+      title: "Admissions",
+      link: "/admin/admissions",
+      icon: "/images/sidebar/admission.svg",
+      selected: false,
+   },
+   {
+      title: "Roadmaps",
+      link: "/admin/roadmaps",
+      icon: "/images/sidebar/roadmap.svg",
+      selected: false,
+   },
+   {
+      title: "Projects",
+      link: "/admin/projects",
+      icon: "/images/sidebar/project.svg",
+      selected: false,
+   },
+   {
+      title: "Academics",
+      link: "/admin/academics",
+      icon: "/images/sidebar/academics.svg",
+      selected: false,
+   },
+   {
+      title: "Career Aware",
+      link: "/admin/career-aware",
+      icon: "/images/sidebar/careeraware.svg",
+      selected: false,
+   },
+   {
+      title: "Find PG",
+      link: "/admin/find-pg",
+      icon: "/images/sidebar/findpg.svg",
+      selected: false,
+   },
+   {
+      title: "Easy Buy",
+      link: "/admin/easy-buy",
+      icon: "/images/sidebar/easybuy.svg",
+      selected: false,
+   },
 ];
 
 const App = () => {
-  const location = useLocation();
+   const location = useLocation();
 
-  const userDetails = getUserDetails();
+   const userDetails = getUserDetails();
 
-  let [user, setUser] = useState(userDetails);
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
-  const [selectSideBar, setSelectSideBar] = useState("Home");
-  if (!user) {
-    return <Home setUser={setUser} />;
-  } else {
-    const isAdmin = user.isAdmin && location.pathname.includes("admin");
-    return (
-      <StyledContainer>
-        <Navbar />
-        <StyledMain className="main">
-          <SideBar
-            sideData={isAdmin ? sideDataForAdmin : sideDataForUser}
-            title={selectSideBar}
-            setTitle={setSelectSideBar}
-          ></SideBar>
+   let [user, setUser] = useState(userDetails);
+   useEffect(() => {
+      window.scrollTo(0, 0);
+   }, [location]);
+   const [selectSideBar, setSelectSideBar] = useState("Home");
+   if (!user) {
+      return <Home setUser={setUser} />;
+   } else {
+      const isAdmin = user.isAdmin && location.pathname.includes("admin");
+      return (
+         <StyledContainer>
+            <Navbar />
+            <StyledMain className="main">
+               <SideBar
+                  sideData={isAdmin ? sideDataForAdmin : sideDataForUser}
+                  title={selectSideBar}
+                  setTitle={setSelectSideBar}
+               ></SideBar>
 
-          <Content>
-            <Routes>
-              <Route path="/" element={<Navigate to="/dashboard" />} />
-              {UserRoutes}
-              {isAdmin ? (
-                AdminRoutes
-              ) : (
-                <Route
-                  path="/admin/*"
-                  element={<Navigate to="/unauthorised" />}
-                />
-              )}
-              {DefaultRoutes}
-            </Routes>
-            <ToastContainer />
-          </Content>
-        </StyledMain>
-      </StyledContainer>
-    );
-  }
+               <Content>
+                  <Routes>
+                     <Route path="/" element={<Navigate to="/dashboard" />} />
+                     {UserRoutes}
+                     {isAdmin ? (
+                        AdminRoutes
+                     ) : (
+                        <Route
+                           path="/admin/*"
+                           element={<Navigate to="/unauthorised" />}
+                        />
+                     )}
+                     {DefaultRoutes}
+                  </Routes>
+                  <ToastContainer />
+               </Content>
+            </StyledMain>
+         </StyledContainer>
+      );
+   }
 };
 
 export default App;
--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -5,23 +5,23 @@ import Tags from "../../styledComponents/Tags/Tags";
 import { MainContent } from "../../styledComponents/common/Common/Common.styles";
 
 const AdminDashboard = () => {
-  const tags = [
-    { value: "All", selected: true },
-    { value: "Interest", selected: false },
-    { value: "Interest", selected: false },
-    { value: "Interest", selected: false },
-    { value: "Interest", selected: false },
-  ];
-  return (
-    <>
-      <MainContent direction="column" flex={3}>
-        <Tags tags={tags} />
-        <h3>Dashboard</h3>
-      </MainContent>
+   const tags = [
+      { value: "All", selected: true },
+      { value: "Interest", selected: false },
+      { value: "Interest", selected: false },
+      { value: "Interest", selected: false },
+      { value: "Interest", selected: false },
+   ];
+   return (
+      <>
+         <MainContent direction="column" flex={3}>
+            <Tags tags={tags} />
+            <h3 style={{ padding: "1rem 0" }}>This is Admin Dashboard</h3>
+         </MainContent>
 
-      <RightSideBar heading="" content={[]} />
-    </>
-  );
+         <RightSideBar heading="" content={[]} />
+      </>
+   );
 };
 
 export default AdminDashboard;
