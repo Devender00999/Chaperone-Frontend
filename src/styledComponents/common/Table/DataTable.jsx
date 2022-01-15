1f7642f25b92c464edffdbd57aaf924bd3495013
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-15 09:52:47 +0530

    fixed PGPage error

--- src/styledComponents/common/Table/DataTable.jsx
+++ src/styledComponents/common/Table/DataTable.jsx
@@ -3,74 +3,84 @@ import EditRoundedIcon from "@mui/icons-material/EditRounded";
 import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
 
 import {
-  TableRow,
-  TableColumn,
-  TableColumnHeading,
-  TableContainer,
+   TableRow,
+   TableColumn,
+   TableColumnHeading,
+   TableContainer,
 } from "./DataTable.styles";
 
 const DataTable = (props) => {
-  const { data, onEdit, onDelete, page } = props;
-  return data.length > 0 ? (
-    <TableContainer>
-      <TableRow style={{ background: "#ff6600", color: "#FFF" }}>
-        <TableColumnHeading style={{ flex: 4 }}>Title</TableColumnHeading>
-        <TableColumnHeading style={{ flex: 3 }}>{page === "Academics" ? "Subject Name" : "Author"}</TableColumnHeading>
-        <TableColumnHeading
-          style={{ flex: 1, display: props.likes === false ? "none" : "block" }}
-        >
-          Likes
-        </TableColumnHeading>
-        <TableColumnHeading style={{ flex: 1 }}>Edit</TableColumnHeading>
-        <TableColumnHeading style={{ flex: 1 }}>Delete</TableColumnHeading>
-      </TableRow>
-      {data.map((blog, id) => {
-        return (
-          <TableRow key={id}>
-            <TableColumn style={{ flex: 4 }}>{blog.topname ? blog.topname : blog.heading} </TableColumn>
-            <TableColumn style={{ flex: 3 }}>{page === "Academics" ? blog.subName : blog.author}</TableColumn>
-            <TableColumn
-              style={{
-                flex: 1,
-                display: props.likes === false ? "none" : "block",
-              }}
+   console.log("props", props.id);
+   const { data, onEdit, onDelete, page } = props;
+   return data.length > 0 ? (
+      <TableContainer>
+         <TableRow style={{ background: "#ff6600", color: "#FFF" }}>
+            <TableColumnHeading style={{ flex: 4 }}>Title</TableColumnHeading>
+            <TableColumnHeading style={{ flex: 3 }}>
+               {page === "Academics" ? "Subject Name" : "Author"}
+            </TableColumnHeading>
+            <TableColumnHeading
+               style={{
+                  flex: 1,
+                  display: props.likes === false ? "none" : "block",
+               }}
             >
-              {blog.likes}
-            </TableColumn>
-            <TableColumn
-              style={{
-                flex: 1,
-                textAlign: "center",
-                padding: "0 !important",
-              }}
-            >
-              <EditRoundedIcon
-                className="pointer-cursor"
-                onClick={() => onEdit(blog._id, props._id)}
-              />
-            </TableColumn>
-            <TableColumn
-              style={{
-                flex: 1,
-                textAlign: "center",
-                padding: "0 !important",
-                justifyContent: "center",
-              }}
-            >
-              <DeleteForeverRoundedIcon
-                className="pointer-cursor"
-                onClick={() => onDelete(blog._id, props._id)}
-              />
-            </TableColumn>
-          </TableRow>
-        );
-      })}
-    </TableContainer>
-  ) : (
-    <h4 style={{ color: "#aaa", textAlign: "center", padding: "1rem 0" }}>
-      No Data Available to Show
-    </h4>
-  );
+               Likes
+            </TableColumnHeading>
+            <TableColumnHeading style={{ flex: 1 }}>Edit</TableColumnHeading>
+            <TableColumnHeading style={{ flex: 1 }}>Delete</TableColumnHeading>
+         </TableRow>
+         {data.map((blog, id) => {
+            return (
+               <TableRow key={id}>
+                  <TableColumn style={{ flex: 4 }}>
+                     {blog.topname ? blog.topname : blog.heading}{" "}
+                  </TableColumn>
+                  <TableColumn style={{ flex: 3 }}>
+                     {page === "Academics" ? blog.subName : blog.author}
+                  </TableColumn>
+                  <TableColumn
+                     style={{
+                        flex: 1,
+                        display: props.likes === false ? "none" : "block",
+                     }}
+                  >
+                     {blog.likes}
+                  </TableColumn>
+                  <TableColumn
+                     style={{
+                        flex: 1,
+                        textAlign: "center",
+                        padding: "0 !important",
+                     }}
+                  >
+                     <EditRoundedIcon
+                        className="pointer-cursor"
+                        onClick={() => onEdit(blog._id, props.id)}
+                     />
+                  </TableColumn>
+                  <TableColumn
+                     style={{
+                        flex: 1,
+                        textAlign: "center",
+                        padding: "0 !important",
+                        justifyContent: "center",
+                     }}
+                  >
+                     <DeleteForeverRoundedIcon
+                        className="pointer-cursor"
+                        onClick={() => onDelete(blog._id, props.id)}
+                     />
+                  </TableColumn>
+               </TableRow>
+            );
+         })}
+      </TableContainer>
+   ) : (
+      <h4 style={{ color: "#aaa", textAlign: "center", padding: "1rem 0" }}>
+         No Data Available to Show
+      </h4>
+   );
 };
 
 export default DataTable;
--- src/styledComponents/styledPages/PGPage/PGPage.jsx
+++ src/styledComponents/styledPages/PGPage/PGPage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Carousel from "../../../components/Carousel/Carousel";
 import {
-  CommonContainer,
-  Content,
-  DescText,
-  Heading,
-  List,
-  ListContainer,
-  ListItem,
-  MainContent,
-  Price,
+   CommonContainer,
+   Content,
+   DescText,
+   Heading,
+   List,
+   ListContainer,
+   ListItem,
+   MainContent,
+   Price,
 } from "../../common/Common/Common.styles";
 import RightSideBar from "../../SidePanel/RightSideBar";
 import { BlogContent, BlogContainer } from "../Blog/Blog.styles";
@@ -22,100 +22,104 @@ import { pgData } from "../../../data/pgFinder";
 import { useParams } from "react-router-dom";
 
 const PGPage = (props) => {
-  const params = useParams();
-  const { pgId } = params;
+   const params = useParams();
+   const { pgId } = params;
 
-  const pgDetails = pgData.find((pg) => pg.id === pgId);
+   const pgDetails = pgData.find((pg) => pg.id === pgId);
+   console.log(pgId, pgDetails);
 
-  return (
-    <Content>
-      <MainContent direction="column" flex={3}>
-        <BlogContainer>
-          <Carousel
-            images={pgDetails.images}
-            height="250px"
-            bottom="4rem"
-            right="1.25rem"
-          />
+   return (
+      <Content>
+         <MainContent direction="column" flex={3}>
+            <BlogContainer>
+               <Carousel
+                  images={pgDetails.images}
+                  height="250px"
+                  bottom="4rem"
+                  right="1.25rem"
+               />
 
-          <BlogContent>
-            <PGContainer>
-              <Heading style={{ color: "#ff6600", fontWeight: 500 }}>
-                {pgDetails.name}
-              </Heading>
-              <Price>₹ {pgDetails.price} </Price>
-            </PGContainer>
-            <DescText>{pgDetails.address}</DescText>
-            <PGContainer
-              style={{
-                justifyContent: "flex-end",
-                padding: "0rem 0 0.5rem",
-              }}
-            >
-              <StyledLink link={pgDetails.location} title="View on Map" />
-            </PGContainer>
+               <BlogContent>
+                  <PGContainer>
+                     <Heading style={{ color: "#ff6600", fontWeight: 500 }}>
+                        {pgDetails.name}
+                     </Heading>
+                     <Price>₹ {pgDetails.price} </Price>
+                  </PGContainer>
+                  <DescText>{pgDetails.address}</DescText>
+                  <PGContainer
+                     style={{
+                        justifyContent: "flex-end",
+                        padding: "0rem 0 0.5rem",
+                     }}
+                  >
+                     <StyledLink
+                        link={pgDetails.location}
+                        title="View on Map"
+                     />
+                  </PGContainer>
 
-            <PGContainer
-              style={{
-                justifyContent: "flex-start",
-              }}
-            >
-              <PGFeature>
-                <SchoolIcon
-                  style={{
-                    fontSize: "1.25rem",
-                    color: "#515151",
-                    marginRight: "0.5rem",
-                  }}
-                />{" "}
-                {pgDetails.distFromCollege}
-              </PGFeature>
+                  <PGContainer
+                     style={{
+                        justifyContent: "flex-start",
+                     }}
+                  >
+                     <PGFeature>
+                        <SchoolIcon
+                           style={{
+                              fontSize: "1.25rem",
+                              color: "#515151",
+                              marginRight: "0.5rem",
+                           }}
+                        />{" "}
+                        {pgDetails.distFromCollege}
+                     </PGFeature>
 
-              <PGFeature>
-                <TrainIcon
-                  style={{
-                    fontSize: "1.25rem",
-                    color: "#515151",
-                    marginRight: "0.5rem",
-                  }}
-                />{" "}
-                {pgDetails.distFromMetro}
-              </PGFeature>
-            </PGContainer>
-            <CommonContainer style={{ justifyContent: "space-between" }}>
-              <ListContainer>
-                <Heading>Amenities</Heading>
-                <List>
-                  {pgDetails.amenities.map((amenity, key) => (
-                    <ListItem key={key}>{amenity}</ListItem>
-                  ))}
-                </List>
-              </ListContainer>
+                     <PGFeature>
+                        <TrainIcon
+                           style={{
+                              fontSize: "1.25rem",
+                              color: "#515151",
+                              marginRight: "0.5rem",
+                           }}
+                        />{" "}
+                        {pgDetails.distFromMetro}
+                     </PGFeature>
+                  </PGContainer>
+                  <CommonContainer style={{ justifyContent: "space-between" }}>
+                     <ListContainer>
+                        <Heading>Amenities</Heading>
+                        <List>
+                           {pgDetails.amenities.map((amenity, key) => (
+                              <ListItem key={key}>{amenity}</ListItem>
+                           ))}
+                        </List>
+                     </ListContainer>
 
-              <ListContainer>
-                <Heading>House Rules</Heading>
-                <List>
-                  {pgDetails.houseRules.map((houseRule, key) => (
-                    <ListItem key={key}>{houseRule}</ListItem>
-                  ))}
-                </List>
-              </ListContainer>
+                     <ListContainer>
+                        <Heading>House Rules</Heading>
+                        <List>
+                           {pgDetails.houseRules.map((houseRule, key) => (
+                              <ListItem key={key}>{houseRule}</ListItem>
+                           ))}
+                        </List>
+                     </ListContainer>
 
-              <ListContainer>
-                <Heading>Other charges</Heading>
-                <List>
-                  {pgDetails.otherCharges.map((otherCharge, key) => (
-                    <ListItem key={key}>{otherCharge}</ListItem>
-                  ))}
-                </List>
-              </ListContainer>
-            </CommonContainer>
-          </BlogContent>
-        </BlogContainer>
-      </MainContent>
-      <RightSideBar heading="" content={[]} />
-    </Content>
-  );
+                     <ListContainer>
+                        <Heading>Other charges</Heading>
+                        <List>
+                           {pgDetails.otherCharges.map((otherCharge, key) => (
+                              <ListItem key={key}>{otherCharge}</ListItem>
+                           ))}
+                        </List>
+                     </ListContainer>
+                  </CommonContainer>
+               </BlogContent>
+            </BlogContainer>
+         </MainContent>
+         <RightSideBar heading="" content={[]} />
+      </Content>
+   );
 };
 
 export default PGPage;
