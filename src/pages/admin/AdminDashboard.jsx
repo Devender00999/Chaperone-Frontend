bc322edc81c27aca45a31a336624f562a5d76888
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-01-15 12:22:14 +0530

    fixed admission

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,39 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import RightSideBar from "../../styledComponents/SidePanel/RightSideBar";
 import Tags from "../../styledComponents/Tags/Tags";
 import { MainContent } from "../../styledComponents/common/Common/Common.styles";
-import DataTable from "../../styledComponents/common/Table/DataTable";
 
 const AdminDashboard = () => {
-   const tags = [
-      { value: "All", selected: true },
-      { value: "Interest", selected: false },
-      { value: "Interest", selected: false },
-      { value: "Interest", selected: false },
-      { value: "Interest", selected: false },
-   ];
+  const tags = [
+    { value: "All", selected: true },
+    { value: "Interest", selected: false },
+    { value: "Interest", selected: false },
+    { value: "Interest", selected: false },
+    { value: "Interest", selected: false },
+  ];
+  return (
+    <>
+      <MainContent direction="column" flex={3}>
+        <Tags tags={tags} />
+        <h3>Dashboard</h3>
+      </MainContent>
 
-   const [allUsers, setAllUsers] = useState();
-   useEffect(() => {
-      async function getAllUsers() {}
-   });
-
-   return (
-      <>
-         <MainContent direction="column" flex={3}>
-            <DataTable
-            //  key={roadmap._id}
-            //  id={roadmap._id}
-            //  likes={false}
-            //  data={roadmap.projects}
-            //  onEdit={handleEdit}
-            //  onDelete={handleDelete}
-            />
-         </MainContent>
-         <RightSideBar heading="" content={[]} />
-      </>
-   );
+      <RightSideBar heading="" content={[]} />
+    </>
+  );
 };
 
 export default AdminDashboard;
