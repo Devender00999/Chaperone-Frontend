39368a9c3b8d46e1be646737895d30dbacc37a93
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-06-03 00:28:14 +0530

    fixed rerendering problem

--- src/pages/admin/admission/AdmissionBlogs.jsx
+++ src/pages/admin/admission/AdmissionBlogs.jsx
@@ -4,11 +4,11 @@ import { Link, useNavigate } from "react-router-dom";
 import DataTable from "../../../styledComponents/common/Table/DataTable";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  PageHeading,
-  HeadingContainer,
-  HeadingContent,
-  MainContent,
-  PrimaryButton,
+    PageHeading,
+    HeadingContainer,
+    HeadingContent,
+    MainContent,
+    PrimaryButton,
 } from "../../../styledComponents/common/Common/Common.styles";
 import { toast } from "react-toastify";
 
@@ -17,62 +17,66 @@ import * as admissionActions from "../../../store/admissions";
 // import { useDispatch } from "react-redux";
 
 const AdmissionBlogs = () => {
-  const [query, setQuery] = useState("");
-  const admissions = useSelector(admissionActions.filteredArticles(query));
-  const dispatch = useDispatch();
-  const navigator = useNavigate();
+    const [query, setQuery] = useState("");
+    const [apiCalled, setApiCalled] = useState(false);
+    const admissions = useSelector(admissionActions.filteredArticles(query));
+    const dispatch = useDispatch();
+    const navigator = useNavigate();
 
-  const handleEdit = (id) => {
-    const article = admissions.find((article) => article._id === id);
-    console.log(article._id);
-    dispatch(admissionActions.selectArticle(article._id));
-    navigator(id);
-  };
-  // let [admissionBlogs, setAdmissionBlogs] = useState(admissions);
+    const handleEdit = (id) => {
+        const article = admissions.find((article) => article._id === id);
+        console.log(article._id);
+        dispatch(admissionActions.selectArticle(article._id));
+        navigator(id);
+    };
 
-  useEffect(() => {
-    if (admissions && admissions.length < 1)
-      dispatch(admissionActions.loadArticles());
-  }, [dispatch, admissions]);
+    useEffect(() => {
+        if (!apiCalled && admissions.length === 0) {
+            dispatch(admissionActions.loadArticles());
+            setApiCalled(true);
+        }
+    }, [dispatch, admissions, apiCalled]);
 
-  const handleDelete = async (id) => {
-    try {
-      dispatch(admissionActions.removeArticle(id));
-      toast.success("Admission article deleted.");
-    } catch (ex) {
-      toast.error(ex.response.message);
-    }
-  };
-  return (
-    <>
-      <MainContent direction={"column"} flex={"auto"}>
-        <HeadingContainer>
-          <PageHeading>Edit Your Admission Blogs</PageHeading>
-          <HeadingContent>
-            <Form.Control
-              style={{ width: "200px", outline: "none" }}
-              type="search"
-              placeholder="Search"
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-            />
-            <Link
-              style={{ textDecoration: "none", color: "White" }}
-              to="/admin/admissions/new"
-            >
-              <PrimaryButton className="primaryButton">Add New</PrimaryButton>
-            </Link>
-          </HeadingContent>
-        </HeadingContainer>
-        <DataTable
-          data={admissions}
-          onEdit={handleEdit}
-          onDelete={handleDelete}
-          page="Admission"
-        />
-      </MainContent>
-    </>
-  );
+    const handleDelete = async (id) => {
+        try {
+            dispatch(admissionActions.removeArticle(id));
+            toast.success("Admission article deleted.");
+        } catch (ex) {
+            toast.error(ex.response.message);
+        }
+    };
+    return (
+        <>
+            <MainContent direction={"column"} flex={"auto"}>
+                <HeadingContainer>
+                    <PageHeading>Edit Your Admission Blogs</PageHeading>
+                    <HeadingContent>
+                        <Form.Control
+                            style={{ width: "200px", outline: "none" }}
+                            type="search"
+                            placeholder="Search"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <Link
+                            style={{ textDecoration: "none", color: "White" }}
+                            to="/admin/admissions/new"
+                        >
+                            <PrimaryButton className="primaryButton">
+                                Add New
+                            </PrimaryButton>
+                        </Link>
+                    </HeadingContent>
+                </HeadingContainer>
+                <DataTable
+                    data={admissions}
+                    onEdit={handleEdit}
+                    onDelete={handleDelete}
+                    page="Admission"
+                />
+            </MainContent>
+        </>
+    );
 };
 
 export default AdmissionBlogs;
--- src/pages/user/Admission.jsx
+++ src/pages/user/Admission.jsx
@@ -1,41 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { MainContent } from "../../styledComponents/common/Common/Common.styles";
 import RightSideBar from "../../styledComponents/SidePanel/RightSideBar";
 import BlogsCard from "../../styledComponents/BlogsCard/BlogsCard";
 import { useDispatch, useSelector } from "react-redux";
 import * as admissionActions from "../../store/admissions";
-const Admission = (props) => {
-  const dispatch = useDispatch();
 
-  const rightSideBarData = {
+const rightSideBarData = {
     heading: "Quick Links",
     content: [
-      "Choice filling Round 1 for B Tech...",
-      "Final Datesheet for Reappear exam",
-      "Datesheet Mid Term exams",
-      "Scholarship Program",
+        "Choice filling Round 1 for B Tech...",
+        "Final Datesheet for Reappear exam",
+        "Datesheet Mid Term exams",
+        "Scholarship Program",
     ],
-  };
-  const admissionArticles = useSelector((state) => state.admissions.articles);
-  const loading = useSelector((state) => state.admissions.loading);
+};
 
-  useEffect(() => {
-    if (admissionArticles.length === 0)
-      dispatch(admissionActions.loadArticles());
-  },[admissionArticles, dispatch]);
+const Admission = () => {
+    const dispatch = useDispatch();
+    const [apiCalled, setApiCalled] = useState(false);
 
-  return (
-    <>
-      <MainContent direction="column" flex={3}>
-        {loading === false
-          ? admissionArticles.map((blog, id) => (
-              <BlogsCard type="admission" key={id} {...blog} />
-            ))
-          : "Loading"}
-      </MainContent>
-      <RightSideBar {...rightSideBarData} />
-    </>
-  );
+    const admissionArticles = useSelector((state) => state.admissions.articles);
+    const loading = useSelector((state) => state.admissions.loading);
+
+    useEffect(() => {
+        if (admissionArticles.length === 0 && !apiCalled) {
+            dispatch(admissionActions.loadArticles());
+            setApiCalled(true);
+        }
+    }, [admissionArticles, apiCalled, dispatch]);
+
+    return (
+        <>
+            <MainContent direction="column" flex={3}>
+                {loading === false
+                    ? admissionArticles.length > 0
+                        ? admissionArticles.map((blog, id) => (
+                              <BlogsCard type="admission" key={id} {...blog} />
+                          ))
+                        : "No data found"
+                    : "Loading"}
+            </MainContent>
+            <RightSideBar {...rightSideBarData} />
+        </>
+    );
 };
 
 export default Admission;
