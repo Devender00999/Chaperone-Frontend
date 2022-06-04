5cf679542e5b4b927bf33ba48cf7b1ecbadb982e
Devender Kumar <Devender00999@gmail.com>
2022-06-04 16:45:23 +0530

    Updated Config file in frontend

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
 const config = {
-   apiUrl: "http://192.168.1.122:3002/api",
-   url: "http://192.168.1.122:3002",
-   // apiUrl: "http://localhost:3002/api",
-   // url: "http://localhost:3002",
-   token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mjg1MTRiNjVjOGQyNGIyZGJhZTIwMDAiLCJuYW1lIjoiRGV2ZW5kZXIgS3VtYXIiLCJlbWFpbCI6ImRldmVuZGVyMDA5OTlAZ21haWwuY29tIiwicHJvZmlsZVBpYyI6Ii9wcm9maWxlUGljcy9wcm9maWxlUGljLTE2NTI4ODg3NTgxNDUtNjk0NDM2NjEyLmpwZyIsInJvbGUiOjAsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1Mjk0NDY5OX0.eztRFqNVmVecLnX4UcxfNiNl5aBRkQUiWYpMPqADpyc",
+   apiUrl: process.env["REACT_APP_CHAPERONE_API"],
+   url: process.env["REACT_APP_CHAPERONE_URL"],
 };
 
+console.log(process.env);
 export const colors = {
    primary: "#ff6600",
    primaryLight: "#ff751a",
