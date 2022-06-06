987a2cbeda0c7eebaf0980324fa6c121403c6c0f
Devender Kumar <Devender00999@gmail.com>
2022-06-07 01:03:09 +0530

    Updated HomePage

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -3,20 +3,14 @@ import React from "react";
 import RightSideBar from "../../styledComponents/SidePanel/RightSideBar";
 import Tags from "../../styledComponents/Tags/Tags";
 import { MainContent } from "../../styledComponents/common/Common/Common.styles";
+import { Alert } from "@mui/material";
 
 const AdminDashboard = () => {
-   const tags = [
-      { value: "All", selected: true },
-      { value: "Interest", selected: false },
-      { value: "Interest", selected: false },
-      { value: "Interest", selected: false },
-      { value: "Interest", selected: false },
-   ];
    return (
       <>
          <MainContent direction="column" flex={3}>
-            <Tags tags={tags} />
             <h3 style={{ padding: "1rem 0" }}>This is Admin Dashboard</h3>
+            <Alert>Nothing to see here. </Alert>
          </MainContent>
 
          <RightSideBar />
