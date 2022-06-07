f6ecff47ec78de89083d480570ea40754fa1194f
kushdeepwalia <skushdeep5@gmail.com>
2022-06-07 17:49:22 +0530

    Console added

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,44 +4,45 @@ import http from "./httpService";
 import config from "../config";
 
 const apiEndPoint = config.apiUrl + "/users/auth";
+console.log(apiEndPoint, config.apiUrl);
 const tokenKey = "token";
 
 http.setJwt(getJwt());
 
 export async function login(email, password) {
-  const { data } = await http.post(apiEndPoint, { email, password });
-  localStorage.setItem(tokenKey, data.token);
+   const { data } = await http.post(apiEndPoint, { email, password });
+   localStorage.setItem(tokenKey, data.token);
 }
 
 export function loginWithJwt(jwt) {
-  localStorage.setItem(tokenKey, jwt);
+   localStorage.setItem(tokenKey, jwt);
 }
 
 export function getJwt() {
-  const token = localStorage.getItem(tokenKey);
-  if (token) return token;
-  return "";
+   const token = localStorage.getItem(tokenKey);
+   if (token) return token;
+   return "";
 }
 
 export function logout() {
-  localStorage.removeItem(tokenKey);
+   localStorage.removeItem(tokenKey);
 }
 
 export function getCurrentUser() {
-  try {
-    const jwt = localStorage.getItem(tokenKey);
-    return jwtDecode(jwt);
-  } catch (ex) {
-    return null;
-  }
+   try {
+      const jwt = localStorage.getItem(tokenKey);
+      return jwtDecode(jwt);
+   } catch (ex) {
+      return null;
+   }
 }
 
 const auth = {
-  login,
-  loginWithJwt,
-  logout,
-  getCurrentUser,
-  getJwt,
+   login,
+   loginWithJwt,
+   logout,
+   getCurrentUser,
+   getJwt,
 };
 
 export default auth;
